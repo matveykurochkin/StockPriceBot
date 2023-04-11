8b8529566bd256fd821e7da901cacd917c53e407
Matvey Kurochkin <matvey.kurochkin@gmail.com>
2023-04-11 19:29:22 +0300

    create class for buttons on bot

---
+++ StockPrice/StockPrice/Internal/BotButtons.cs
@@ -0,0 +1,38 @@
+﻿using Telegram.Bot.Types.ReplyMarkups;
+
+namespace StockPrice.Internal
+{
+    static internal class BotButtons
+    {
+        public static IReplyMarkup MainButtonOnBot()
+        {
+            var tgButton = new ReplyKeyboardMarkup(new[]
+            {
+        new[]
+        {
+            new KeyboardButton("Список популярных акций 💵"),
+        }
+         });
+            tgButton.ResizeKeyboard = true;
+            return tgButton;
+        }
+        public static IReplyMarkup MostPopularStock()
+        {
+            var tgButton = new ReplyKeyboardMarkup(new[]
+            {
+        new[]
+        {
+            new KeyboardButton("AAPL"),
+            new KeyboardButton("YNDX"),
+            new KeyboardButton("TSLA")
+        },
+        new[]
+        {
+            new KeyboardButton("Назад ⬅")
+        }
+         });
+            tgButton.ResizeKeyboard = true;
+            return tgButton;
+        }
+    }
+}
