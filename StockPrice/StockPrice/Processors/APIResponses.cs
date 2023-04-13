e63f82847405bb149d32c5de47f5acfd2145b8ab
Matvey Kurochkin <matvey.kurochkin@gmail.com>
2023-04-13 16:40:47 +0300

    create class for API response

---
+++ StockPrice/StockPrice/Processors/APIResponses.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace StockPrice.Processors
+{
+    public class ExchangeRateData
+    {
+        [JsonProperty("rates")]
+        public Dictionary<string, double>? Rates { get; set; }
+    }
+}
